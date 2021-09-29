e71d4df1d7a971d331317c212990be38aeea9e36
Zecevic Nemanja <zecevicn1@gmail.com>
2021-09-29 13:16:24 +0200

    Za pregledanje

---
+++ script.js
@@ -0,0 +1,29 @@
+let btn = document.querySelector(".claim-free-trial-btn");
+
+btn.addEventListener("click", () => {
+    let inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+        if (input.parentNode.classList.contains("wrong")) {
+            input.parentNode.classList.remove("wrong");
+        }
+        if (input.value.trim() === "") {
+            input.parentNode.classList.add("wrong");
+        }
+        let email = document.querySelector(".email");
+        if (email.value.trim !== "") {
+            ValidateEmail(email);
+        }
+    })
+});
+
+function ValidateEmail(email) {
+    let emailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!email.value.match(emailformat)) {
+        email.parentNode.classList.add("wrong");
+    };
+
+};
+
+
+
+
