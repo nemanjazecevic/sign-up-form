1a36d9766d915a31021d28f24956d481f93e84e1
Zecevic Nemanja <zecevicn1@gmail.com>
2021-09-30 10:09:07 +0200

    Ispravka zahteva

--- script.js
+++ script.js
@@ -6,20 +6,25 @@ btn.addEventListener("click", () => {
         if (input.parentNode.classList.contains("wrong")) {
             input.parentNode.classList.remove("wrong");
         }
+        if (input.parentNode.classList.contains("error-email")) {
+            input.parentNode.classList.remove("error-email");
+        }
         if (input.value.trim() === "") {
             input.parentNode.classList.add("wrong");
         }
-        let email = document.querySelector(".email");
-        if (email.value.trim !== "") {
-            ValidateEmail(email);
-        }
+
     })
+    let email = document.querySelector(".email");
+    if (email.value !== "") {
+
+        ValidateEmail(email);
+    }
 });
 
 function ValidateEmail(email) {
     let emailformat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!email.value.match(emailformat)) {
-        email.parentNode.classList.add("wrong");
+        email.parentNode.classList.add("error-email");
     };
 
 };
